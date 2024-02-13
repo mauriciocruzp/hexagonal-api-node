6d3ca5fd04a2fe81608b0a6cb45c735eb2599d2c
Mauricio Cruz <mawcp7@gmail.com>
2024-02-13 14:59:38 -0600

    Add RegisterUserController

---
+++ src/user-management/infrastructure/controllers/RegisterUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { RegisterUserUseCase } from "../../application/use-cases/RegisterUserUseCase";
+
+export class RegisterUserController {
+
+    constructor(readonly useCase: RegisterUserUseCase) {}
+
+    async execute(req: Request,res: Response) {
+        try{
+
+            let user = await this.useCase.execute(req.body);
+
+            return res.status(201).json(user);
+
+        }catch(error:any) {
+            return res.status(error.http_status ?? 500)
+                .json({
+                    message:"Error to register user.",
+                    error:error
+                });
+        }
+    }
+
+}
