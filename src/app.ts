3bd3c6b08772117c0025f9a2e6f725f8fc35437f
Mauricio Cruz <mawcp7@gmail.com>
2024-02-13 14:48:11 -0600

    Add app.ts file with server setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { Signale } from "signale";
+import morgan from 'morgan';
+
+import { userManagementRouter } from './user-management/infrastructure/routes/UserManagementRouter';
+
+const app = express();
+const signale = new Signale();
+
+dotenv.config();
+app.use(morgan('dev'));
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use('/user', userManagementRouter);
+
+async function startServer() {app.listen(PORT,() => {
+        signale.success(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+}
+
+startServer();
