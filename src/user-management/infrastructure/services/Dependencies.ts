93efb011009374aabf9537a5588aaad4e7f50b60
Mauricio Cruz <mawcp7@gmail.com>
2024-02-13 20:42:23 -0600

    Add HandleBcrypt dependency

--- src/user-management/infrastructure/services/Dependencies.ts
+++ src/user-management/infrastructure/services/Dependencies.ts
@@ -1,8 +1,9 @@
 import { RegisterUserUseCase } from "../../application/use-cases/register-user.use-case";
 import { MysqlUserRepository } from "../repositories/mysql-user.repository";
 import { RegisterUserController } from "../controllers/register-user.controller";
+import { HandleBcrypt } from "../../application/helpers/handle-bcrypt";
 
 const mysqlUserRepository = new MysqlUserRepository();
 const registerUserUseCase = new RegisterUserUseCase(mysqlUserRepository);
-
+export const handleBCrypt = new HandleBcrypt();
 export const registerUserController = new RegisterUserController(registerUserUseCase);
