43d39d1507a7491891b6b10a347f1e4855aa2647
Mauricio Cruz <mawcp7@gmail.com>
2024-02-13 22:52:12 -0600

    Add users table to database

--- src/database/start-db.sql
+++ src/database/start-db.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `hex-node`.`users` (
+CREATE TABLE `user-management-db`.`users` (
   `id` CHAR(36) NOT NULL,
   `first_name` VARCHAR(40) NOT NULL,
   `last_name` VARCHAR(40) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
   `cellphone` VARCHAR(20) NULL,
   `activation_token` VARCHAR(100) NULL,
   `verification_date` DATETIME NULL,
